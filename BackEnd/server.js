995725c1592df1474cf18ece721aab07a9bdbd0b
GabrielLJanuario <148555743+GabrielLJanuario@users.noreply.github.com>
2026-05-26 15:32:05 -0300

    Comit incial

---
+++ BackEnd/server.js
@@ -0,0 +1,18 @@
+const products = [
+    {
+        id: 1,
+        title: 'Camiseta Nike',
+        price: 80,
+        size: 'M',
+        condition: 'Excelente'
+    },
+    {
+        id: 2,
+        title: 'Calça Jeans',
+        price: 120,
+        size: 'G',
+        condition: 'Boa'
+    }
+]
+    
+console.log(products)       
